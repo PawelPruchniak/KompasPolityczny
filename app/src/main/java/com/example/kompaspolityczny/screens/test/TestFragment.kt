b6b1caf84dae9916f149ff712a1e110795eaa13e
PawelPruchniak <pw.pruchniak@gmail.com>
2020-08-16 12:33:31 +0200

    Ustawienie TestFragment i łączenie go z TestViewModel

--- app/src/main/java/com/example/kompaspolityczny/screens/test/TestFragment.kt
+++ app/src/main/java/com/example/kompaspolityczny/screens/test/TestFragment.kt
@@ -7,26 +7,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.kompaspolityczny.R
+import com.example.kompaspolityczny.databinding.TestFragmentBinding
 
 class TestFragment : Fragment() {
 
     private lateinit var viewModel: TestViewModel
 
+    private lateinit var binding: TestFragmentBinding
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
+
+        // Setting binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.test_fragment, container, false)
+
+        // Setting viewModel
+        viewModel = ViewModelProviders.of(this).get(TestViewModel::class.java)
+
+        binding.testViewModel = viewModel
+        binding.setLifecycleOwner(this)
+
         // Setting ActionBar title
         (activity as AppCompatActivity).supportActionBar?.title = "Test"
 
-        return inflater.inflate(R.layout.test_fragment, container, false)
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(TestViewModel::class.java)
-        // TODO: Use the ViewModel
+        return binding.root
     }
-
 }
--- app/src/main/java/com/example/kompaspolityczny/screens/test/TestViewModel.kt
+++ app/src/main/java/com/example/kompaspolityczny/screens/test/TestViewModel.kt
@@ -1,7 +1,55 @@
 package com.example.kompaspolityczny.screens.test
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TestViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    // The current question
+    private val _question = MutableLiveData<String>()
+    val question: LiveData<String>
+        get() = _question
+
+    // The current question number
+    private val _questionNumber = MutableLiveData<Int>()
+    val questionNumber: LiveData<Int>
+        get() = _questionNumber
+
+    private  lateinit var questionList: MutableList<String>
+
+    init {
+        resetQuestionList()
+        nextQuestion()
+        _questionNumber.value = 0
+    }
+
+    private fun resetQuestionList() {
+        questionList = mutableListOf(
+            "Im bardziej wolny rynek, tym bardziej wolni są ludzie",
+            "Dla każdego według jego potrzeb, od każdego według jego zdolności, to w zasadzie dobry pomysł",
+            "Państwo powinno dążyć do neutralności klimatycznej"
+        )
+    }
+
+    private fun nextQuestion() {
+        if (questionList.isEmpty()) {
+
+        }
+        else{
+            _questionNumber.value = _questionNumber.value?.plus(1)
+            _question.value = questionList.removeAt(0)
+        }
+    }
+
+    private fun previousQuestion() {
+        if (_questionNumber.value!! >= 1){
+            _questionNumber.value = _questionNumber.value?.minus(1)
+        }
+    }
+
+    fun onButtonClick() {
+        nextQuestion()
+    }
+
 }
