a3524918a00ca959a4ba4ac193bedc2dda81bc00
PawelPruchniak <pw.pruchniak@gmail.com>
2020-09-01 19:39:15 +0200

    Dodanie przycisku Clear do HistoryFragment, stworzenie odpowiedniego zarzadzania widzialnościa przycisku.

--- app/src/main/java/com/example/kompaspolityczny/screens/history/HistoryFragmentViewModel.kt
+++ app/src/main/java/com/example/kompaspolityczny/screens/history/HistoryFragmentViewModel.kt
@@ -2,11 +2,36 @@ package com.example.kompaspolityczny.screens.history
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Transformations
 import com.example.kompaspolityczny.database.TestResultDatabaseDao
+import kotlinx.coroutines.*
 
 class HistoryFragmentViewModel(
     val database: TestResultDatabaseDao,
     application: Application): AndroidViewModel(application) {
 
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
     val results = database.getAllResults()
+
+    val clearButtonVisible = Transformations.map(results) {
+        it?.isNotEmpty()
+    }
+
+    private suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+
+    fun onClear() {
+        uiScope.launch {
+            // Clear the database table.
+            clear()
+        }
+        // Show a snackbar message, because it's friendly.
+        //_showSnackbarEvent.value = true
+    }
+
 }
