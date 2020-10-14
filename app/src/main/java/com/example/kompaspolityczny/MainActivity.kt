1c78c88b86d1cb5543e18379ce34c37d66fceb96
PawelPruchniak <pw.pruchniak@gmail.com>
2020-10-14 18:39:45 +0200

    wreszcie XD hamburger dziala

--- app/src/main/java/com/example/kompaspolityczny/MainActivity.kt
+++ app/src/main/java/com/example/kompaspolityczny/MainActivity.kt
@@ -5,11 +5,10 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kompaspolityczny.databinding.ActivityMainBinding
 import net.danlew.android.joda.JodaTimeAndroid
@@ -24,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         // initializing JodaTimeAndroid before using it
         JodaTimeAndroid.init(this)
 
-        binding = setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setupNavigation()
 
@@ -34,10 +33,16 @@ class MainActivity : AppCompatActivity() {
             = navigateUp(findNavController(R.id.nav_host_fragment), binding.drawerLayout)
 
     private fun setupNavigation() {
+        // first find the nav controller
         val navController = findNavController(R.id.nav_host_fragment)
 
         setSupportActionBar(binding.toolbar)
 
+        // then setup the action bar, tell it about the DrawerLayout
+        setupActionBarWithNavController(navController, binding.drawerLayout)
+
+
+        // finally setup the left drawer (called a NavigationView)
         binding.navigationView.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
@@ -54,5 +59,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
 }
